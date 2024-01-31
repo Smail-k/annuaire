8b98be720ba84c5fcfedaf16e43dfde0fe8320eb
unknown <ismail.kouidi@etu.univ-amu.fr>
2024-01-31 15:30:58 +0100

    service proxy update

--- service-proxy/src/main/java/com/polytech/Proxy.java
+++ service-proxy/src/main/java/com/polytech/Proxy.java
@@ -2,7 +2,12 @@ package com.polytech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -10,5 +15,10 @@ public class Proxy {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Proxy.class, args);
+	}
+	
+	@Bean
+	DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+		return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
 	}
 }
